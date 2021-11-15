5ed0d70012e8ca3bc1e6770c33a8f14fbe068385
RachelAya <rachel.ayateke@gmail.com>
2021-11-15 12:33:08 +0200

    Added code to login.php

--- Config.php
+++ Config.php
@@ -1,7 +1,18 @@
 <?php
 
-    define('DB_SERVER', 'localhost:3036');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'library');
-    
+// Database credentials
+// TODO: Check root password
+
+define('DB_SERVER', 'localhost:3036');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'library');
+
+// Connect to MYSQL database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect to database. " . mysqli_connect_error());
+}
+?>
--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 include "db_conn.php";
 
-if(isset($_POST['email']) && isset($_POST['password'])) {
+if(isset($_POST['username']) && isset($_POST['password'])) {
     
     function validate($data) {
         $data = trim($data);
@@ -12,11 +12,11 @@ if(isset($_POST['email']) && isset($_POST['password'])) {
     }
 }
 
-$email = validate($_POST['email']);
+$username = validate($_POST['username']);
 $pass = validate($_POST['password']);
 
-if(empty($email)){
-    header("Location: index.php?error=Email is required");
+if(empty($username)){
+    header("Location: index.php?error=Username is required");
     exit();
 }
 else if(empty($pass)) {
@@ -24,15 +24,28 @@ else if(empty($pass)) {
     exit();
 }
 
-$sql = "SELECT * FROM users WHERE user_id='$email' AND password='$pass'";
+$sql = "SELECT * FROM users WHERE user_name='$username' AND password='$pass'";
 
 $result = mysqli_query($conn, $sql);
 
 if(mysqli_num_rows($result) === 1) {
     $row = mysqli_fetch_assoc($result);
-    if($row['user_name'] === $email && $row['password'] === $pass) {
+    if($row['user_name'] === $username && $row['password'] === $pass) {
         echo "You are logged in.";
-        // Add sessions for all variables https://www.youtube.com/watch?v=scd8YKiuS7I
-            
+        // Add sessions for all variables 
+        $_SESSION['user_name'] = $row['user_name'];
+        // This line might cause errors
+        $_SESSION['name'] = $row['name'];
+        $_SESSION['id'] = $row['id'];
+        header("Location: home.php");
+        exit();
     }
+    else{
+        header("Location: index.php?error=Incorrect Username or password");
+        exit();
+    }
+}
+else{
+    header("Location: index.php");
+    exit();
 }
